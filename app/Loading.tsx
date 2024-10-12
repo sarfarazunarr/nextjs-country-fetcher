545b4246d5e05c3209ecb34026afe7730cf2fce3
Sarfaraz Unar <sarfarazunarr@gmail.com>
2024-10-12 11:14:12 +0500

    loading updated

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const Loading = () => {
-    return (
-        <div className='flex justify-center items-center h-screen w-full'>
-            <div className="loader">
-            </div>
-        </div>
-    )
-}
+const Loading = () => (
+    <div className="flex justify-center items-center h-screen w-full">
+        <div className="loader"></div>
+    </div>
+)
 
 export default Loading
+
